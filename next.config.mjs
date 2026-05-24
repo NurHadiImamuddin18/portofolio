6744b937883d26a5bd1d6afa2fa9004f3d85eb8d
NurHadiImamuddin18 <nurhadiimamuddin@gmail.com>
2026-05-25 06:23:58 +0700

    finall

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // Wajib jika Anda deploy ke GitHub Pages (Static HTML Export)
-  basePath: '/portofolio',
-  assetPrefix: '/portofolio',
-  images: {
-    unoptimized: true, // Wajib diaktifkan karena GitHub Pages tidak mendukung optimasi gambar otomatis Next.js
-  },
-};
+const nextConfig = {};
 export default nextConfig;
